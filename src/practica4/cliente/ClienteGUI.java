45ada2af58864ab00763c2cc1d98ce96861a4fc3
Dani <danielchenelcea@gmail.com>
2021-03-21 17:11:33 +0100

    Inicio de sesión

--- src/practica4/cliente/ClienteGUI.java
+++ src/practica4/cliente/ClienteGUI.java
@@ -5,9 +5,12 @@ import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import practica4.cliente.controladores.ClienteCallbackImpl;
+import practica4.cliente.gui.vInicioSesion.vInicioSesionController;
 import practica4.cliente.gui.vPrincipal.vPrincipal;
 import practica4.interfaces.IUsuario;
 import practica4.interfaces.ServidorCallback;
@@ -17,53 +20,32 @@ import java.rmi.RemoteException;
 import java.util.UUID;
 
 public class ClienteGUI extends Application {
+    final static String host="localhost";
+    final static int porto=19000;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        ServidorCallback servidorCallback = (ServidorCallback) Naming.lookup(String.format("rmi://localhost:19000/servidor"));
-        IUsuario usuario;
 
-        Stage stage=new Stage();
-        stage.setTitle("Práctica 1");
-        stage.setResizable(false);
-
-
-        FXMLLoader fxmlLoader = new FXMLLoader();
-
-
-
-        String u="dani";
-        String p="abc123..";
-
-
-        if(servidorCallback.comprobarUsuario(u,p)){
-            usuario=servidorCallback.getUsuario(u);
-            usuario.setClienteCallback(new ClienteCallbackImpl());
-            System.out.println(usuario.getUuid());
-        }else{
-            System.out.println("Contraseña podre");
+        ServidorCallback servidorCallback;
+        try {
+            servidorCallback = (ServidorCallback) Naming.lookup(String.format("rmi://%s:%d/servidor",host,porto));
+        } catch (Exception ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erro conectandose o servidor");
+            alert.setHeaderText("Erro conectandose o servidor");
+            alert.setContentText(String.format("Non se puido establecer conexión con rmi://%s:%d/servidor",host,porto));
+            alert.getDialogPane().setExpandableContent(new TextArea(ex.getMessage()));
+            alert.showAndWait();
             return;
         }
 
-
-        fxmlLoader.setController(new vPrincipal(servidorCallback,usuario));
-        fxmlLoader.setLocation(getClass().getResource("/practica4/cliente/gui/vPrincipal/vPrincipal.fxml"));
+        Stage stage = new Stage();
+        stage.setTitle("Práctica 4");
+        stage.setResizable(false);
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setController(new vInicioSesionController(servidorCallback));
+        fxmlLoader.setLocation(getClass().getResource("/practica4/cliente/gui/vInicioSesion/vInicioSesion.fxml"));
 
         stage.setScene(new Scene(fxmlLoader.load()));
-        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent t) {
-                try {
-                    if(usuario.isRegistrado()){
-                        usuario.setRegistrado(false);
-                        servidorCallback.desRegistrarCliente(usuario);
-                    }
-                } catch (RemoteException e) {
-
-                }
-                Platform.exit();
-                System.exit(0);
-            }
-        });
         stage.show();
     }
 }
--- src/practica4/cliente/gui/vInicioSesion/vInicioSesionController.java
+++ src/practica4/cliente/gui/vInicioSesion/vInicioSesionController.java
@@ -1,4 +1,99 @@
 package practica4.cliente.gui.vInicioSesion;
 
-public class vInicioSesionController {
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import practica4.cliente.controladores.ClienteCallbackImpl;
+import practica4.cliente.gui.vPrincipal.vPrincipal;
+import practica4.interfaces.IUsuario;
+import practica4.interfaces.ServidorCallback;
+import practica4.servidor.obxectos.Usuario;
+
+import java.io.IOException;
+import java.net.URL;
+import java.rmi.RemoteException;
+import java.util.ResourceBundle;
+
+public class vInicioSesionController implements Initializable {
+    private ServidorCallback servidorCallback;
+    private Stage stage;
+    private IUsuario usuario;
+    private FXMLLoader fxmlLoader;
+
+    @FXML
+    private TextField tfNomeUsuario;
+    @FXML
+    private TextField tfContrasinal;
+    @FXML
+    private Label lblContrasinalIncorrecto;
+
+
+    public vInicioSesionController(ServidorCallback servidorCallback) {
+        this.servidorCallback = servidorCallback;
+        this.fxmlLoader = new FXMLLoader();
+        this.stage=new Stage();
+    }
+
+    @FXML
+    private void iniciarSesionClick(ActionEvent event) throws Exception {
+        String u = tfNomeUsuario.getText();
+        String p = tfContrasinal.getText();
+        if (p.length() == 0 || u.length() == 0) {
+            return;
+        }
+
+        if (!servidorCallback.comprobarUsuario(u, p)) {
+            lblContrasinalIncorrecto.setVisible(true);
+            System.out.println("Contraseña podre");
+            return;
+        }
+        iniciarSesion(u);
+
+        ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+    }
+
+
+    private void iniciarSesion(String  u) throws IOException {
+        usuario = servidorCallback.getUsuario(u);
+        usuario.setClienteCallback(new ClienteCallbackImpl());
+        fxmlLoader.setController(new vPrincipal(servidorCallback,usuario));
+        fxmlLoader.setLocation(getClass().getResource("/practica4/cliente/gui/vPrincipal/vPrincipal.fxml"));
+        stage.setScene(new Scene(fxmlLoader.load()));
+        stage.setTitle("Práctica 4: "+ u);
+        stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                try {
+                    if(usuario!=null && usuario.isRegistrado()){
+                        usuario.setRegistrado(false);
+                        servidorCallback.desRegistrarCliente(usuario);
+                    }
+                } catch (RemoteException e) {
+                    System.out.println(e);
+                }
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+        stage.show();
+
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        //BORRAR ESTOOOOOOOOOOOOOOO
+        tfNomeUsuario.setText("dani");
+        tfContrasinal.setText("abc123..");
+    }
 }
